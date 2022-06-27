f95698ba60b7a4beb02a3f85f663908f6ca80510
pedroeborges <48721021+pedroeborges@users.noreply.github.com>
2022-06-27 17:45:07 -0300

    Tarefa 01

---
+++ Lets Code - Java/Cap01/DaysOfWeek.java
@@ -0,0 +1,5 @@
+package Cap01;
+
+public enum DaysOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ Lets Code - Java/Cap01/Test.java
@@ -0,0 +1,24 @@
+package Cap01;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome");
+        final String nome = sc.nextLine();
+        System.out.println("Digite a idade");
+        final int age = sc.nextInt();
+    }
+
+    BigDecimal resultado = new BigDecimal("0.01");
+    String nome = "Pedro";
+
+    int teste = (1 & 0);
+
+}
